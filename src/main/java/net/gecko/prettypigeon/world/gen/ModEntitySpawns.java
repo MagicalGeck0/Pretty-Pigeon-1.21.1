db656fe7eb2b6fbfa2647a6465c405d34524d969
MagicalGeck0 <clement.ronan.bellec@gmail..com>
2025-07-08 21:04:53 +0200

    changed pigeon spawn

--- src/main/java/net/gecko/prettypigeon/world/gen/ModEntitySpawns.java
+++ src/main/java/net/gecko/prettypigeon/world/gen/ModEntitySpawns.java
@@ -8,12 +8,13 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnLocationTypes;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.BiomeKeys;
 
 public class ModEntitySpawns {
     public static void addSpawns() {
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.FLOWER_FOREST, BiomeKeys.BIRCH_FOREST, BiomeKeys.WINDSWEPT_FOREST, BiomeKeys.PLAINS, BiomeKeys.DARK_FOREST),
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(),
                 SpawnGroup.CREATURE, ModEntities.PIGEON, 60, 2, 7);
         SpawnRestriction.register(ModEntities.PIGEON, SpawnLocationTypes.ON_GROUND,
                 Heightmap.Type.MOTION_BLOCKING, AnimalEntity::isValidNaturalSpawn);
